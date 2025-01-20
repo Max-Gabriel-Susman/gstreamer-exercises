74e7c53a718f4f45fdc0d9e7dd85da3cc18a2014
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-01-20 11:25:11 -0700

    first commit for gstreamer basic pipeline exercise

---
+++ GStreamerBasicPipeline/GStreamerBasicPipeline/GStreamerBasicPipeline.cpp
@@ -0,0 +1,12 @@
+﻿// GStreamerBasicPipeline.cpp : Defines the entry point for the application.
+//
+
+#include "GStreamerBasicPipeline.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
